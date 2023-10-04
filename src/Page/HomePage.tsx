954128869d64a33c51f654e9e83f2a43877c3c83
unknown <marine.richer41@gmail.com>
2023-10-04 08:09:20 +0200

    switch pc

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -1,4 +1,7 @@
 
+// import css
+import styles from "./../Style/Page/HomePage.module.css";
+
 //import component menu / nav
 import Nav from "../Component/Nav.tsx";
 
@@ -6,6 +9,14 @@ function HomePage() {
     return(
         <>
          <Nav />
+            <div className={styles.container}>
+                <div className={styles.containerName}>
+                    <h1>MARINE RICHER</h1>
+                </div>
+                <div className={styles.containerContent}>
+                    <h1>BTS SIO option SLAM</h1>
+                </div>
+            </div>
         </>
     )
 }
